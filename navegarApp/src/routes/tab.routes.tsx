0cea5b43ef2013ef5d2396299efd6140d2de45cb
iEduardoD3V <es270142@gmail.com>
2023-10-11 11:16:48 -0300

    feat:tela lista add

--- navegarApp/src/routes/tab.routes.tsx
+++ navegarApp/src/routes/tab.routes.tsx
@@ -1,36 +1,37 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import {Feather} from '@expo/vector-icons'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
-import Inicial from '../screens/TelaInicial'
-import Detalhes from '../screens/TelaDetalhes'
-import Config from '../screens/TelaConfig'
+import Inicial from "../screens/TelaInicial";
+import Detalhes from "../screens/TelaDetalhes";
+import Config from "../screens/TelaConfig";
+import Lista from "../screens/TelaDetalhes";
 
 const Tab = createBottomTabNavigator();
 
-export default function TabRoutes(){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen
-                name='Inicio'
-                component={Inicial}
-                options={{
-                    tabBarIcon:() => <Feather name='home' size={25}/>
-                }}
-            />
-            <Tab.Screen
-                name='Detalhes'
-                component={Detalhes}
-                options={{
-                    tabBarIcon:() => <Feather name='heart' size={25}/>
-                }}
-            />
-            <Tab.Screen
-                name='Configuração'
-                component={Config}
-                options={{
-                    tabBarIcon:() => <Feather name='settings' size={25}/>
-                }}
-            />
-        </Tab.Navigator>
-    )
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Inicio"
+        component={Inicial}
+        options={{
+          tabBarIcon: () => <Feather name="home" size={25} />,
+        }}
+      />
+      <Tab.Screen
+        name="Lista"
+        component={Lista}
+        options={{
+          tabBarIcon: () => <Feather name="list" size={25} />,
+        }}
+      />
+      <Tab.Screen
+        name="Configuração"
+        component={Config}
+        options={{
+          tabBarIcon: () => <Feather name="settings" size={25} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
--- navegarApp/src/screens/TelaDetalhes.tsx
+++ navegarApp/src/screens/TelaDetalhes.tsx
@@ -1,23 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Button,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
-
-export default function Detalhes() {
+const [inputTexto, setInputTexto] = useState<string>("");
+export default function Lista() {
   return (
-    <View style={styles.container}>
-      <Text style={{fontWeight:'bold', fontSize:50, color:'#FF0000'}}>Tela Detalhes</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 40, fontWeight: "bold" }}>Lista de Compras</Text>
       <StatusBar style="auto" />
-    </View>
+      <View>
+        <TextInput
+          value={inputTexto}
+          style={{
+            borderWidth: 2,
+            borderColor: "#000",
+            width: 200,
+            marginTop: 20,
+            marginBottom: 20,
+            textAlign: "center",
+            fontSize: 20,
+            fontWeight: "bold",
+            borderRadius: 10,
+          }}
+        />
+      </View>
+
+      <View style={styles.boxLista}>
+        <FlatList
+          data={[
+            { key: "Banana" },
+            { key: "Café" },
+            { key: "Aveia" },
+            { key: "Carne" },
+          ]}
+          renderItem={({ item }) => <Text style={styles.list}>{item.key}</Text>}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
-
+  boxLista: {
+    width: 400,
+    height: 500,
+    borderRadius: 15,
+    marginTop: 60,
+    backgroundColor: "#B0C4DE",
+    borderColor: "#1c1c1c",
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  list: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 5,
+  },
 });
-
